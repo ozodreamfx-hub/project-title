53996c4763d6c88c4bbe5530d0b54fcc1f4ecf25
ozodreamfx-hub <ozodreamfx@gmail.com>
2026-08-14 18:19:19 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,79 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pano-final1",
+      "name": "pano final1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1334759676550323,
+          "pitch": 0.055552280303050594,
+          "rotation": 0,
+          "target": "1-pano2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-pano2",
+      "name": "pano2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.346894552006278,
+          "pitch": 0.10140024367634837,
+          "rotation": 3.141592653589793,
+          "target": "0-pano-final1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
